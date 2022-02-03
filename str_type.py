0d3a4278ffc66fbaec46919a9c5d827dc309a746
Jean Baptiste Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-02-03 14:13:53 -0500

    File loop.py added

---
+++ str_type.py
@@ -0,0 +1,20 @@
+'This is a string'
+"This is also a string"
+
+print(
+"""
+This is a multiline
+A multiline
+""")
+
+
+print(
+    """
+    This is a \" and a \' in a string
+    """
+)
+
+print('A \\ in a string')
+
+path = r'C:\Users\Prince\Documents\Spells'
+print(path)
