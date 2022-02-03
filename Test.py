5043ec6c887359f62b110dec28370747dcf522d7
Jean Baptiste Prince Stanley Lovensky J <ingjeanbaptiste@gmail.com>
2022-02-03 13:46:00 -0500

    File loop.py added

--- Test.py
+++ Test.py
@@ -4,3 +4,37 @@ n = 5
 k = 3
 print(fac(n) + fac(k))
 
+number = int(3)
+number = int(4.2)
+
+print(number)
+
+print(0x10)
+
+print(0o10)
+
+print(0b10)
+
+print(int('0110',2))
+
+print(None)
+
+print(True) ; print(False) ;
+print(bool(1.3))
+print(bool(0.0)) ; print(bool(-1))
+
+print(5==5)
+
+if True:
+    print("It's true")
+
+n = 5
+k = 5
+
+if n==k:
+    print(True)
+else:
+    print(False)
+nom=None
+
+
---
+++ loop.py
@@ -0,0 +1,20 @@
+c = 4;
+while c!=0:
+    print(c)
+    if c==2:
+        print('On y est presque')
+    c -=1
+    if c==0:
+        print('c\'est bon')
+
+
+
+
+nom = None
+
+while nom!='prince':
+    nom = input('> Entre un nom : \n')
+    print(nom)
+    if nom=='prince':
+        print('Well done.')
+
